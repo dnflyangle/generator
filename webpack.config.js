c378a82550358aa65fb877abd87c0e9c7c39d8b6
Isabella Li <yiman.isabella@gmail.com>
2018-04-06 09:59:00 +1000

    fix babel start, using nodemon to track file changes

--- webpack.config.js
+++ webpack.config.js
@@ -27,7 +27,7 @@ module.exports = {
       },
       {
         test: /\.js$/,
-        include: path.join(__dirname, '/lambdas'),
+        include: path.join(__dirname, '/src'),
         exclude: /node_modules/,
         loader: 'babel-loader',
       },
