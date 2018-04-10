b24399dea6e452927d31858d79026d6452ff60ad
Isabella Li <yiman.isabella@gmail.com>
2018-04-10 15:42:41 +1000

    Add ejs template. end to end flow finished! yay!

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
+import ejs from 'ejs';
+import fs from 'fs';
+
 import buildMeetupUrls from './src/buildMeetupUrls';
 import getMeetupEvents from './src/getMeetupEvents';
 import filterEvents from './src/filterEvents';
 import groupEvents from './src/groupEvents';
+import logger from './src/utils/logger';
 
 const main = async (startDateOfWeek) => {
   const meetupUrls = buildMeetupUrls();
@@ -10,8 +14,21 @@ const main = async (startDateOfWeek) => {
 
   const filteredEvents = filterEvents(events, startDateOfWeek);
 
-  console.log(JSON.stringify(groupEvents(filteredEvents, startDateOfWeek)));
+  const groupedEvents = groupEvents(filteredEvents, startDateOfWeek);
+
+  fs.readFile(`${__dirname}/emailTemplate.ejs`, 'utf8', (err, data) => {
+    if (err) {
+      logger.error(err);
+    }
+    const ejsString = data;
+    const template = ejs.compile(ejsString);
+    const html = template({ groupedEvents });
+    fs.writeFile('output.html', html, (error) => {
+      if (error) {
+        logger.error(error);
+      }
+    });
+  });
 };
 
 main('2018-04-08');
-
