fdd919e17a2945c62455f8942787e25892dd5489
Isabella Li <yiman.isabella@gmail.com>
2018-05-22 15:03:44 +1000

    Revert "test the timezone"
    This reverts commit 0e4f91ac1b2d33e84cc7694bb9d9812b43ccea0f.

--- app.js
+++ app.js
@@ -22,7 +22,6 @@ mongoose
 
 app.get('/generate', async (req, res) => {
   try {
-    logger.info(moment().toString());
     await generateMeetupHtml(moment().startOf('week').format('YYYY-MM-DD'));
     res.status(200).send('content generated, please check output.html');
   } catch (err) {
