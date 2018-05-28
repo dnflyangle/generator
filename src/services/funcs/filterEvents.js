66784a80f7f1e9c756fe699872eb842f32144b78
Isabella Li <yiman.isabella@gmail.com>
2018-05-28 11:26:35 +1000

    add two logger to trouble shoot duplicate sunday bug

--- src/services/funcs/filterEvents.js
+++ src/services/funcs/filterEvents.js
@@ -1,9 +1,12 @@
 import { flatten, filter, map, uniqBy } from 'lodash';
 import moment from 'moment';
+import logger from '../../../src/utils/logger';
 
 const filterEvents = (events, startDateOfWeek) => {
   const startDate = moment(startDateOfWeek);
   const endDate = moment(startDateOfWeek).add(6, 'days');
+  logger.info(startDate);
+  logger.info(endDate);
 
   const filteredEvents = filter(flatten(events), event =>
     moment(event.local_date).isSameOrAfter(startDate) &&
