85b8b90ad671d2344b86ab4fcbe2d24d5f093d09
Issy <yiman.isabella@gmail.com>
2019-09-05 09:20:48 +1000

    upgrade to babel 7 and jest 25

---
+++ babel.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  presets: [
+    ['@babel/preset-env',
+      {
+        targets: {
+          edge: '17',
+          firefox: '60',
+          chrome: '67',
+          safari: '11.1',
+        },
+        corejs: '2',
+        useBuiltIns: 'usage',
+      }],
+  ],
+  plugins: [
+    [
+      '@babel/plugin-transform-runtime',
+      {
+        regenerator: true,
+      },
+    ],
+    ['@babel/plugin-proposal-object-rest-spread'],
+  ],
+};
